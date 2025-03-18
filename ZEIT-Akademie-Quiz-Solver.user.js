26e7be6dfb72f6f8c412181bb9c2e48ac533780e
Tosox <57193602+Tosox@users.noreply.github.com>
2025-03-18 23:23:19 +0100

    chore: Update username

--- ZEIT-Akademie-Quiz-Solver.user.js
+++ ZEIT-Akademie-Quiz-Solver.user.js
@@ -1,16 +1,16 @@
 // ==UserScript==
 // @name            ZEIT Akademie Quiz Solver
-// @author          TosoxDev
-// @namespace       https://github.com/TosoxDev/ZEIT-Akademie-Quiz-Solver
-// @homepage        https://github.com/TosoxDev/ZEIT-Akademie-Quiz-Solver
-// @supportURL      https://github.com/TosoxDev/ZEIT-Akademie-Quiz-Solver/issues
-// @updateURL       https://github.com/TosoxDev/ZEIT-Akademie-Quiz-Solver/releases/latest/download/ZEIT-Akademie-Quiz-Solver.user.js
-// @downloadURL     https://github.com/TosoxDev/ZEIT-Akademie-Quiz-Solver/releases/latest/download/ZEIT-Akademie-Quiz-Solver.user.js
-// @icon            https://github.com/TosoxDev/ZEIT-Akademie-Quiz-Solver/blob/master/readme-res/icon.png?raw=true
+// @author          Tosox
+// @namespace       https://github.com/Tosox/ZEIT-Akademie-Quiz-Solver
+// @homepage        https://github.com/Tosox/ZEIT-Akademie-Quiz-Solver
+// @supportURL      https://github.com/Tosox/ZEIT-Akademie-Quiz-Solver/issues
+// @updateURL       https://github.com/Tosox/ZEIT-Akademie-Quiz-Solver/releases/latest/download/ZEIT-Akademie-Quiz-Solver.user.js
+// @downloadURL     https://github.com/Tosox/ZEIT-Akademie-Quiz-Solver/releases/latest/download/ZEIT-Akademie-Quiz-Solver.user.js
+// @icon            https://github.com/Tosox/ZEIT-Akademie-Quiz-Solver/blob/master/assets/icon.png?raw=true
 // @description     Shows the solutions to the current quiz questions
-// @version         1.0.1
+// @version         1.0.2
 // @license         MIT
-// @copyright       Copyright (c) 2023 TosoxDev
+// @copyright       Copyright (c) 2025 Tosox
 // @match           https://www.zeitakademie.de/*
 // @grant           GM_registerMenuCommand
 // @grant           unsafeWindow
@@ -20,60 +20,58 @@
     "use strict";
 
     function getCurrentSolution() {
-        var oQuiz = unsafeWindow.oQuiz
+        var oQuiz = unsafeWindow.oQuiz;
         if (!oQuiz) {
-            return null
+            return null;
         }
 
-        var currentIndex = oQuiz.quizSession.activeQuestionIndex
+        var currentIndex = oQuiz.quizSession.activeQuestionIndex;
         if (!oQuiz.quizValues.questions[currentIndex]) {
-            return null
+            return null;
         }
 
-        var answers = oQuiz.quizValues.questions[currentIndex].answers
-        return answers.find(a => a.correctAnswer)
+        var answers = oQuiz.quizValues.questions[currentIndex].answers;
+        return answers.find(a => a.correctAnswer);
     }
 
     GM_registerMenuCommand("Show Solution (S)", function() {
         var solution = getCurrentSolution()
         if (!solution) {
-            alert("There is no active quiz at the moment")
-            return
+            alert("There is no active quiz at the moment");
+            return;
         }
 
-        alert(`Answer ${solution.id + 1}: ${solution.answer}`)
-    }, 's')
+        alert(`Answer ${solution.id + 1}: ${solution.answer}`);
+    }, 's');
 
     GM_registerMenuCommand("Solve Quiz (Q)", function() {
-        var solution = getCurrentSolution()
+        var solution = getCurrentSolution();
         if (!solution) {
-            alert("There is no active quiz at the moment")
-            return
+            alert("There is no active quiz at the moment");
+            return;
         }
 
         function clickAnswers() {
             setTimeout(function() {
-                var solution = getCurrentSolution()
+                var solution = getCurrentSolution();
                 if (!solution) {
-                    return
+                    return;
                 }
 
-                var btnAnswer = document.getElementById(`button${solution.id}`)
+                var btnAnswer = document.getElementById(`button${solution.id}`);
                 if (btnAnswer) {
-                    btnAnswer.click()
+                    btnAnswer.click();
                 }
 
-                var btnNext = document.getElementById("buttonNextCard")
+                var btnNext = document.getElementById("buttonNextCard");
                 if (btnNext) {
-                    btnNext.click()
+                    btnNext.click();
                 }
 
-                clickAnswers()
-            }, 1500)
+                clickAnswers();
+            }, 1500);
         }
 
-        clickAnswers()
-
-    }, 'q')
-
+        clickAnswers();
+    }, 'q');
 })()
