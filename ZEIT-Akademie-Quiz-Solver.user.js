e38dee01ff246d3be105ccbad19c743eb90648a3
Tosox <57193602+Tosox@users.noreply.github.com>
2025-03-19 00:06:35 +0100

    refactor: Use GM_notification

--- ZEIT-Akademie-Quiz-Solver.user.js
+++ ZEIT-Akademie-Quiz-Solver.user.js
@@ -8,67 +8,76 @@
 // @downloadURL     https://github.com/Tosox/ZEIT-Akademie-Quiz-Solver/releases/latest/download/ZEIT-Akademie-Quiz-Solver.user.js
 // @icon            https://github.com/Tosox/ZEIT-Akademie-Quiz-Solver/blob/master/assets/icon.png?raw=true
 // @description     Shows the solutions to the current quiz questions
-// @version         1.0.2
+// @version         1.0.3
 // @license         MIT
 // @copyright       Copyright (c) 2025 Tosox
 // @match           https://www.zeitakademie.de/*
 // @grant           GM_registerMenuCommand
+// @grant           GM_notification
 // @grant           unsafeWindow
 // ==/UserScript==
 
-(function(){
+(function() {
     "use strict";
 
     function getCurrentSolution() {
-        var oQuiz = unsafeWindow.oQuiz;
+        const oQuiz = unsafeWindow.oQuiz;
         if (!oQuiz) {
             return null;
         }
 
-        var currentIndex = oQuiz.quizSession.activeQuestionIndex;
+        const currentIndex = oQuiz.quizSession.activeQuestionIndex;
         if (!oQuiz.quizValues.questions[currentIndex]) {
             return null;
         }
 
-        var answers = oQuiz.quizValues.questions[currentIndex].answers;
+        const answers = oQuiz.quizValues.questions[currentIndex].answers;
         return answers.find(a => a.correctAnswer);
     }
 
     GM_registerMenuCommand("Show Solution (S)", function() {
-        var solution = getCurrentSolution()
+        const solution = getCurrentSolution()
         if (!solution) {
-            alert("There is no active quiz at the moment");
+            GM_notification({
+                text: "There is no active quiz at the moment",
+                timeout: 3000
+            });
             return;
         }
 
-        alert(`Answer ${solution.id + 1}: ${solution.answer}`);
+        GM_notification({
+            text: `Answer ${Number(solution.id) + 1}: ${solution.answer}`,
+            timeout: 3000
+        });
     }, 's');
 
     GM_registerMenuCommand("Solve Quiz (Q)", function() {
-        var solution = getCurrentSolution();
+        const solution = getCurrentSolution();
         if (!solution) {
-            alert("There is no active quiz at the moment");
+            GM_notification({
+                text: "There is no active quiz at the moment",
+                timeout: 3000
+            });
             return;
         }
 
         function clickAnswers() {
             setTimeout(function() {
-                var solution = getCurrentSolution();
+                const solution = getCurrentSolution();
                 if (!solution) {
                     return;
                 }
 
-                var btnAnswer = document.getElementById(`button${solution.id}`);
+                const btnAnswer = document.getElementById(`button${solution.id}`);
                 if (btnAnswer) {
                     btnAnswer.click();
                 }
 
-                var btnNext = document.getElementById("buttonNextCard");
+                const btnNext = document.getElementById("buttonNextCard");
                 if (btnNext) {
                     btnNext.click();
+                    clickAnswers();
                 }
-
-                clickAnswers();
             }, 1500);
         }
 
